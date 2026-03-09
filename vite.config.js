40660cc7c3b0398ecbbed2a1490ebbe8c972db5a
Qasim shahzad <qasimshahzad087417@gmail.com>
2026-03-09 23:41:04 +0500

    some changes

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  // THIS IS THE MOST IMPORTANT LINE FOR DEPLOYMENT
-  base: './', 
-});
+  plugins: [
+    tailwindcss(),
+  ],
+})
